30862cf2de3e3a3f537b1dd3d9e96a40c74d8d25
ZartexVertagen <tanakorndoi05@hotmail.com>
2025-01-23 18:02:11 +0700

    Switch JS to bootstap, add animation to dropdown.

--- src/public/javascripts/script.js
+++
@@ -1,7 +0,0 @@
-function more_items() {
-    var element = document.getElementById("dropdown-item")
-    if (element.classList.contains("hidden"))
-        element.classList.remove("hidden")
-    else
-        element.classList.add("hidden")
-}
