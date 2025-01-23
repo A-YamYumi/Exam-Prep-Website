245eb17e5ec5895d33bf231a7b00b1bccc99f2d6
ZartexVertagen <tanakorndoi05@hotmail.com>
2025-01-23 14:36:53 +0700

    Fixed class hidden function

--- src/public/javascripts/script.js
+++ src/public/javascripts/script.js
@@ -1,6 +1,6 @@
 function more_items() {
     var element = document.getElementById("dropdown-item")
-    if (element.className === "hidden")
+    if (element.classList.contains("hidden"))
         element.classList.remove("hidden")
     else
         element.classList.add("hidden")
