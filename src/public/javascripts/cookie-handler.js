7694563e23d7820954946b8e9468e332bc1809f5
Petch Hongkaew <Krittitee.Petch@gmail.com>
2025-03-02 02:59:12 +0700

    merge

--- src/public/javascripts/cookie-handler.js
+++ src/public/javascripts/cookie-handler.js
@@ -12,7 +12,7 @@ function setupNavbar() {
     profile = document.getElementById("user-login")
     if (profile != null) {
         if (user == '1') {
-            profile.innerHTML = `<a href="#" class="d-inline bi bi-bell text-white"></a>
+            profile.innerHTML = `<a href="/user/notification" class="d-inline bi bi-bell text-white"></a>
             <a href="/user/setting" class="d-inline text-white mx-2 text-decoration-none">User_name</a>
                         <a href="#" class="d-inline bi bi-person text-white"></a>`
         } else {
