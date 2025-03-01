b3382b703351b46103624cf57473635711a91504
Petch Hongkaew <Krittitee.Petch@gmail.com>
2025-03-02 01:58:50 +0700

    setting

--- src/public/javascripts/cookie-handler.js
+++ src/public/javascripts/cookie-handler.js
@@ -8,7 +8,7 @@ profile = document.getElementById("user-login")
 if (profile != null) {
     if (user == '1') {
         profile.innerHTML = `<a href="#" class="d-inline bi bi-bell text-white"></a>
-        <a href="/user/profile" class="d-inline text-white mx-2 text-decoration-none">User_name</a>
+        <a href="/user/setting" class="d-inline text-white mx-2 text-decoration-none">User_name</a>
                     <a href="#" class="d-inline bi bi-person text-white"></a>`
     } else {
         profile.innerHTML = `
